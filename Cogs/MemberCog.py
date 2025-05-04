de26fe72b8681f665d8ee2abd47ac2318bf74363
Mike Sweeney <blindx@gmail.com>
2025-05-04 15:17:12 -0400

    Move to semantical config keys

--- Cogs/MemberCog.py
+++ Cogs/MemberCog.py
@@ -5,17 +5,17 @@ from discord import app_commands
 from discord.ext import commands, tasks
 import ConfigDB as config
 
-def check_member_age(member: discord.Member, new_account_days: int):
+def check_member_age(member: discord.Member, new_account_day_count: int):
     now = datetime.datetime.now(datetime.timezone.utc)
     diff = now - member.created_at
 
-    return diff.days >= new_account_days
+    return diff.days >= new_account_day_count
 
-def check_server_age(member: discord.Member, member_hours: int):
+def check_server_age(member: discord.Member, member_hour_count: int):
     now = datetime.datetime.now(datetime.timezone.utc)
     diff = now - member.joined_at
 
-    return (diff.total_seconds() / 3600) >= member_hours
+    return (diff.total_seconds() / 3600) >= member_hour_count
 
 class MemberCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -33,7 +33,7 @@ class MemberCog(commands.Cog):
         for server in self.bot.guilds:
             configs = await config.get_configs(server.id, [
                 'new_account_role',
-                'new_account_days',
+                'new_account_day_count',
             ])
 
             try:
@@ -41,7 +41,7 @@ class MemberCog(commands.Cog):
                 if role:
                     members = [m for m in server.members if role in m.roles]
                     for member in members:
-                        if check_member_age(member, configs.new_account_days):
+                        if check_member_age(member, configs.new_account_day_count):
                             await member.remove_roles(role)
             except Exception as e:
                 print('Failed to check new account roles:')
@@ -55,7 +55,7 @@ class MemberCog(commands.Cog):
             configs = await config.get_configs(server.id, [
                 'new_account_role',
                 'member_role',
-                'member_hours'
+                'member_hour_count'
             ])
 
             try:
@@ -65,7 +65,7 @@ class MemberCog(commands.Cog):
                 if role:
                     members = [m for m in server.members if role not in m.roles]
                     for member in members:
-                        if check_server_age(member, configs.member_hours) \
+                        if check_server_age(member, configs.member_hour_count) \
                         and new_acct_role not in member.roles:
                             await member.add_roles(role)
             except Exception as e:
@@ -77,10 +77,10 @@ class MemberCog(commands.Cog):
     async def check_new_member_age(self, member: discord.Member):
         configs = await config.get_configs(member.guild.id, [
             'new_account_role',
-            'new_account_days',
+            'new_account_day_count',
         ])
 
-        if not check_member_age(member, configs.new_account_days):
+        if not check_member_age(member, configs.new_account_day_count):
             role = member.guild.get_role(configs.new_account_role)
             await member.add_roles(role)
 
